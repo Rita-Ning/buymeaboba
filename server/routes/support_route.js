3600477b7d0c7a643e067aad203adcf7adc7365a
Tang Ning <>
2022-06-02 14:10:30 +0800

    style: route name

--- server/routes/support_route.js
+++ server/routes/support_route.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const { wrapAsync } = require('../../util/util');
 
-const tappayController = require('../controllers/support_controllers');
-const linepayController = require('../controllers/linepay_controller');
+const TappayController = require('../controllers/support_controllers');
+const LinepayController = require('../controllers/linepay_controller');
 
-router.post('/support/checkout', wrapAsync(tappayController.tappay));
-router.post('/linepay', wrapAsync(linepayController.linepay));
-router.post('/linepay/check', wrapAsync(linepayController.linepayCheck));
+router.post('/support/checkout', wrapAsync(TappayController.tappay));
+router.post('/linepay', wrapAsync(LinepayController.linepay));
+router.post('/linepay/check', wrapAsync(LinepayController.linepayCheck));
 
 module.exports = router;
