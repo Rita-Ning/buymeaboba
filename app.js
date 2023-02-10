f884eaa3c974fc98f39eb1159a6aaaed429e5ff6
Tang Ning <>
2023-02-10 15:58:17 +0800

    :fix: .env example

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require('./util/socket-chat').campaign(server);
 app.set('trust proxy', true);
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('./public'));
 
